d7321460a8db2f924f9085b43449f87fe54f0646
Kristel Lamaj <kri.lamaj@gmail.com>
2021-02-23 18:00:49 +0200

    Plug-in Base File

--- external-orders-management.php
+++ external-orders-management.php
@@ -2,11 +2,28 @@
     /**
      * Plugin Name: External Orders Management System
      * Plugin URI:  https://huntsmanstudio.gr/external-orders-management/
-     * Description: This plug-in is used for sending orders data to an external orders management system when an order id created
+     * Description: This plug-in is used for sending orders data to an external orders management system when an order id created and the customer is navigates to thank you page
      * Version:     1.0
      * Author:      Developers Huntsman Studio
      * Author URI:  https://huntsmanstudio.gr/
     */
 
-    
+    // Includes
+    include './includes/functions.php';
+
+    // Add Action
+    add_action('woocommerce_new_order', 'order');
+
+    function order( $order_id ){
+
+        // Store Customer
+        customer($order_id);
+
+        // Store Order
+        order($order_id);
+
+        // Order items
+        
+
+    }
 ?>
