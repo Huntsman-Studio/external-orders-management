d633c420575672edfb2466e84e6eb02d1f862119
Kristel Lamaj <kri.lamaj@gmail.com>
2021-02-23 13:15:38 +0200

    Plugin Details

---
+++ external-orders-management.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Plugin Name: External Orders Management System
+     * Plugin URI:  https://huntsmanstudio.gr/external-orders-management/
+     * Description: This plug-in is used for sending orders data to an external orders management system when an order id created
+     * Version:     1.0
+     * Author:      Developers Huntsman Studio
+     * Author URI:  https://huntsmanstudio.gr/
+    */
+
+    
+?>
