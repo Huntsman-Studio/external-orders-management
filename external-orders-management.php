06731a0b68b0d3a0b3e5275e3ff7bd06fc7faaac
klamaj <kri.lamaj@gmail.com>
2021-04-08 12:40:30 +0300

    BAse Plugin File

--- external-orders-management.php
+++ external-orders-management.php
@@ -3,38 +3,46 @@
      * Plugin Name: External Orders Management System
      * Plugin URI:  https://huntsmanstudio.gr/external-orders-management/
      * Description: This plug-in is used for sending orders data to an external orders management system when an order id created and the customer is navigates to thank you page
-     * Version:     1.0
+     * Version:     2.0
      * Author:      Developers Huntsman Studio
      * Author URI:  https://huntsmanstudio.gr/
     */
 
     // Includes
-    include 'includes/functions.php';
+    include 'includes/order_details.php';
+    include 'includes/order.php';
+    include 'includes/console_log.php';
+    include 'includes/customer.php';
     include 'includes/DB/db_config.php';
+    include 'includes/items.php';
     include 'includes/setters_getters.php';
-    include 'includes/wc-partially-completed.php';
+    include_once 'includes/wc-partially-completed.php';
+    include_once 'includes/wc-disputed.php';
+
 
     // Add Action
-    add_action('woocommerce_thankyou', 'order');
+    add_action('woocommerce_thankyou', 'order_creation');
 
-    function order( $order_id ){
+    function order_creation( $order_id ){
 
         // Connect to DB
         $conn = localCon();
-        $_paypal_fee=$_stripe_payment=$_stripe_fee=0;
 
-        $sql = 'SELECT meta_key, meta_value FROM wp_postmeta WHERE post_id ='.$order_id.'';
+        // Default Variable Values
+        $_paypal_fee = $_stripe_payment = $_stripe_fee = 0;
 
+        // Initialize SQL Query to GET order_data   
+        $sql = 'SELECT meta_key, meta_value FROM wp_postmeta WHERE post_id = ' . $order_id . '';
+        
+        // Get Result
         $res = $conn->query($sql);
 
+        // Check Result
         if($res->num_rows > 0){
             while($row = mysqli_fetch_array($res,MYSQLI_ASSOC)){
-
-                // if($row['meta_key'] === 'VAT'){
-                //     $_vat = $row['meta_value'];
-                // }
                 
                 switch($row['meta_key']){
+                    
                     // First Name
                     case '_billing_first_name':
                         // ucwords()    --> Uppercase the first character of each word in a string
@@ -42,6 +50,7 @@
                         // (string)     --> Convert valkue to String
                         $_name = (string)ucwords(strtolower($row['meta_value']));
                         break;
+
                     // Last Name
                     case '_billing_last_name':
                         // ucwords()    --> Uppercase the first character of each word in a string
@@ -49,19 +58,21 @@
                         // (string)     --> Convert valkue to String
                         $_last = (string)ucwords(strtolower($row['meta_value']));
                         break;
+
                     // Phone
                     case '_billing_phone':
                         // (string)     --> Convert valkue to String
                         $_phone = (string)$row['meta_value'];
                         break;
+
                     // Address
-                    
                     case '_billing_address_1':
                         // ucwords()    --> Uppercase the first character of each word in a string
                         // strtolower() --> Make a string lowercase
                         // (string)     --> Convert valkue to String
                         $_address = (string)ucwords(strtolower($row['meta_value']));
                         break;
+
                     // City
                     case '_billing_city':
                         // ucwords()    --> Uppercase the first character of each word in a string
@@ -69,12 +80,14 @@
                         // (string)     --> Convert valkue to String
                         $_city = (string)ucwords(strtolower($row['meta_value']));
                         break;
+
                     // Postcode
                     case '_billing_postcode':
                         // strtoupper() --> Make a string uppercase
                         // (string)     --> Convert valkue to String
                         $_post = (string)strtoupper($row['meta_value']);
                         break;
+                    
                     // Country
                     case '_billing_country':
                         // meta_value --> New Encypted Country Code
@@ -82,59 +95,73 @@
                         // meta_value --> Full country name (ex: GR --> Greece)
                         $_country = (string)get_billing_country($row['meta_value']);
                         break;
+
                     // VAT
                     case 'VAT':
                         $_vat = (string)$row['meta_value'];
                         break;
+
                     // Business Address
                     case 'Company address':
                         $_bus_ad = (string)$row['meta_value'];
                         break;
+                        
                     // Business Name
                     case 'Company name':
                         $_bus_name = (string)$row['meta_value'];
                         break;
+                        
                     // Business City
                     case '_business_city':
                         $_bus_city = (string)$row['meta_value'];
                         break;
+
                     // Business Post
                     case '_business_post':
                         $_bus_post = (string)$row['meta_value'];
                         break;
+
                     // Business Phone
                     case '_business_phone':
                         $_bus_phone = (string)$row['meta_value'];
                         break;
+
                     // Email
                     case '_billing_email':
                         $_email = (string)$row['meta_value'];
                         break;
+
                     // Payment method
                     case '_payment_method':
                         // Meta_Value --> New String
                         $_payment_method = (string)set_payment_method($row['meta_value']);
                         break;
+
                     // Stripe fee
                     case '_stripe_fee':
                         $_stripe_fee = $row['meta_value'];
                         break;
+
                     // Stripe Payment
                     case '_stripe_net':
                         $_stripe_payment = $row['meta_value'];
                         break;
+
                     // Transaction ID
                     case '_transaction_id':
                         $_trans_id = (string)$row['meta_value'];
                         break;
+
                     // PayPal Fee
                     case '_paypal_transaction_fee':
                         $_paypal_fee = $row['meta_value'];
                         break;
+
                     // PayPal Fee -- PayPal Checkout Plug-in
                     case 'PayPal Transaction Fee':
                         $_paypal_fee = $row['meta_value'];
                         break;
+
                     // Default
                     default:
                         break;
@@ -158,45 +185,39 @@
                 $_notes     = $row['post_excert'];
             }
         }
-        //C2/15934/2021-02-25 11:30:29/On Hold/Bank Transfer//////ofwurttrrtrtligmr@icare.eu/
-
-        // Disconnect from Local
-        $conn->close();
-
-        // Connect to external
-        $conn = openCon();
-
-        // Store customers
-        $sqlCust = 'INSERT INTO customers(name, phone, address, city, post, country, vat, bus_ad, bus_name, bus_city, bus_post, bus_phone, email) 
-            VALUES ("'.$_name.' '.$_last.'","'.$_phone.'","'.$_address.'","'.$_city.'","'.$_post.'","'.$_country.'","'.$_vat.'","'.$_bus_ad.'","'.$_bus_name.'",
-            "'.$_bus_city.'","'.$_bus_post.'","'.$_bus_phone.'","'.$_email.'")';
-
-        // Check if Stored
-        if($conn->query($sqlCust) === TRUE){
-            $test = 'OK Customers | ';
+        //C2/15934/2021-02-25 11:30:29/On Hold/Bank Transfer//////ofwurttrrtrtligmr@icare.eu/);
+    
+        // Check if has VAT
+        if(isset($_vat)){
+            $_inv = 1;
         } else {
-            $test = $conn->error;
+            $_inv = 0;
         }
+        
+        // Store Customer
+        customer($_email);
 
+        // Store order
+        // $_time =  date('h:i:s'); # Set time
+        order($_code.'/'.$order_id, $_date, $_status, $_payment_method, $_notes, $_stripe_fee, $_paypal_fee, $_stripe_payment, $_email, $_trans_id, date('h:i:s'), $_inv);
 
-        // Store orders
-        // $code=$_code.'/'.$order_id;
-        $sqlOrder = 'INSERT INTO orders (id, date, status, payment, notes, stripe_fee, paypal_fee, stripe_payment, email, trans_id) 
-        VALUES ("'.$_code.'/'.$order_id.'", "'.$_date.'", "'.$_status.'", "'.$_payment_method.'", "'.$_notes.'", '.$_stripe_fee.' ,'.$_paypal_fee.', '.$_stripe_payment.', "'.$_email.'", "'.$_trans_id.'")';
+        // Store order details
+        order_details($_code.'/'.$order_id, "_name", $_name.' '.$_last);            # Name
+        order_details($_code.'/'.$order_id, "_phone", $_phone);                     # Phone
+        order_details($_code.'/'.$order_id, "_address", $_address);                 # Address
+        order_details($_code.'/'.$order_id, "_city", $_city);                       # City
+        order_details($_code.'/'.$order_id, "_postcode", $_post);                   # Postcode
+        order_details($_code.'/'.$order_id, "_country", $_country);                 # Country
 
-
-        // Check if Sotored
-        if($conn->query($sqlOrder) === TRUE){
-            $test .= 'OK Order | ';
-        } else {
-            $test = $conn->error;
+        // Check if Has VAT
+        if(isset($_vat)){
+            order_details($_code.'/'.$order_id, "_vat", $_vat);                     # VAT
+            order_details($_code.'/'.$order_id, "_business_name", $_bus_name);      # Business Name
+            order_details($_code.'/'.$order_id, "_business_address", $_bus_ad);     # Business Address
+            order_details($_code.'/'.$order_id, "_business_city", $_bus_city);      # Business City
+            order_details($_code.'/'.$order_id, "_business_post", $_bus_post);      # Business Post
+            order_details($_code.'/'.$order_id, "_business_phone", $_bus_phone);    # Business Phone
         }
-
-        // Dsiconnect From External
-        $conn->close();
-
-        // Connect to local
-        $conn = localCon();
 
         // Store order Items 
         $sqlItem = 'SELECT * FROM wp_woocommerce_order_items WHERE order_id = '.$order_id;
@@ -336,46 +357,13 @@
                             }
                         } // wp_woocommerce_order_itemmeta
                     }
-                }
+                }                
 
-                $string = 'INSERT INTO items(id, order_id, name, qty, total, discount, sku, type)
-                            VALUES("'.$_code.'/'.$itemID.'","'.$_code.'/'.$order_id.'","'.$item_name.'",'.$qty.','.$regular_price.','.$discount.',"'.$sku.'","'.$type.'")';
-
-                store_item($string);
+                items($_code.'/'.$itemID, $_code.'/'.$order_id, $item_name, $qty, $regular_price, $discount, $sku, $type);
 
             } // wp_woocommerce_order_items
         }
-
-        
-        $conn->close();
-        
-        ///////////////////////////////////////////////////////////
-        ///////////////////////////////////////////////////////////  
-
-        // Connect to external
-        $connEx = openCon();
-
-        // Debug INSERT result
-        // $sql1 = 'INSERT INTO test(id) VALUES ("'.$test.'")';
-
-        // if($connEx->query($sql1)){
-
-        // }
-        // ///////////////////////////////////////////////////////////
-        // ///////////////////////////////////////////////////////////
-
-        // // Disconnect from DB 
-        $connEx->close();
+    
+        $conn -> close();
     }
-
-    function store_item($value){
-
-        $conn = openCon();
-
-        if($conn->query($value)){
-
-        }
-
-        $conn->close();
-    }
-?>
+?> 
